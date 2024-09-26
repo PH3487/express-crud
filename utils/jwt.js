831efcba598df5df3a69f5631511248c206eccd2
phanuwat3487 <boyasd12345@gmail.com>
2024-09-27 02:00:33 +0700

    add workflow docker

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,11 +1,21 @@
 const jwt = require("jsonwebtoken");
 const pk = process.env.JWT_PRIVATE_KEY;
 module.exports = {
+  /**
+   *
+   * @param {string|object|Buffer} payload
+   * @returns {string}
+   */
   async sign(payload) {
     return await jwt.sign(payload, pk, {
       expiresIn: "1d",
     });
   },
+  /**
+   *
+   * @param {string} token: string
+   * @returns {*} { uid: string, username: string, iat: number, exp: number }
+   */
   async verify(token) {
     const decoded = await jwt.verify(token, pk);
     if (!decoded || decoded.exp < Date.now() / 1000) {
